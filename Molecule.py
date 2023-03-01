c6660ae3690f8f2c2258f51a0e15faa38bbcff7f
Aidan Harrison <aidharrison10@gmail.com>
2023-03-01 19:48:20 +0000

    Version 0.4
    # ~ Fixed and improved 'put' statement
        # ~ Optimised operator search by only searching for identifiers
        # ~ Operators now support same variable calculations: x + x
        # ~ '++' && '--' operator now function as intended
        # ~ Functions and their arguments are now stored
        # ~ Comments now increase linecount
        # ~ Enums restored to global
        # ~ Converted to format prints

--- Molecule.py
+++ Molecule.py
@@ -2,28 +2,26 @@
 # Aid Harrison
 # 2023
 # Python 3.11.1
-# Verison 0.3 (Alpha release)
-    # ~ Now supports correct raw value calculation
-    # ~ Variable compile now returns the last line to reduce number of iterations
-        # ~ After variable compile, indexes are now skipped
-    # ~ WIPE command now skips the current line
-    # ~ ENUMS used for op code pattern matching
-    # ~ Change from iterator to memory access iterating on core
-    # ~ General code cleanup
-    # ~ Whitespace is now cleared correctly
-    # ~ Optimised comment check
+# Verison 0.4 (Alpha release)
+    # ~ Fixed and improved 'put' statement
+    # ~ Optimised operator search by only searching for identifiers 
+    # ~ Operators now support same variable calculations: x + x
+    # ~ '++' && '--' operator now function as intended
+    # ~ Functions and their arguments are now stored
+    # ~ Comments now increase linecount
+    # ~ Enums restored to global
+    # ~ Converted to format prints
 
 # FIX first operand 0 issue on single operator!
 
 import queue
 import time
 from enum import Enum
-
-functions = {} # function name | function line | Move!
-    # Allows for function overriding
 class STATEMENT_CMDS():
     PUT    = 1
     WIPE   = 2
+    FOR    = 3
+    IF     = 4
 class OPERATOR_TOKENS():
     ADD    = 0
     SUB    = 1
@@ -40,20 +38,42 @@ class OPERATOR_TOKENS():
     RET    = 12
     EQ     = 13
 
+class FUNCTION_RETURN_CODE():
+    INT = 0
+    FLT = 1
+    STR = 2
+
+class FUNCTION_STORAGE():
+    def __init__(self, fn_name : str = "Default FN", argument_list = [], fn_index : int = 0) -> None:
+        self.name : str = fn_name
+        self.arg_list = argument_list
+        self.index : int = fn_index
+        self.body = []
+        self.RETURN_CODE : int = FUNCTION_RETURN_CODE.INT
+
+    def EXECUTE_FUNCTION(self) -> None:
+        ...
+
+    def DEBUG_PRINT(self) -> None:
+        print(f"Name: {self.name}")
+        print(f"Arguments: {self.arg_list}")
+        print(f"Index: {self.index}")
+        print(f"Body: {self.body}")
 class Program:
     def __init__(self, direct : str = "", debugActive : bool = False) -> None:
         # === CORE DATA ===
         self.directory : str = direct
         self.source : str = []
         self.types : str  = ["int", "flt", "str"]
-        self.enumDef = STATEMENT_CMDS()
-        self.OP_TK   = OPERATOR_TOKENS()
+        #self.ST_CD = STATEMENT_CMDS()
+        self.functions : FUNCTION_STORAGE = []
         self.start_index : int = 0
-        self.DEBUG = debugActive
         # === VARIABLES ===
         self.variables = {}
         self.lastType : str = "" # Used for variable compile
         self.operators = ['+', '-', '/', '*', '++', '--', '==', '+=', '-=', '/=', '*=', '->', '<-', '=']
+        # === MISC ===
+        self.DEBUG = debugActive
         # === RUN ===
         self.build(direct)
         if not self.lexer():
@@ -70,9 +90,12 @@ class Program:
             if input_str[i].isalnum():
                 return_str += input_str[i]
         return return_str
+    
+    def ERROR_CHECK(self) -> bool:
+        ...
 
     def function_call(self, index : int) -> bool:
-        print(self.source[index])
+        print(f"{self.source[index]}")
         return False
 
     def variable_compile(self) -> bool:
@@ -83,7 +106,7 @@ class Program:
             self.source[line] = self.source[line].strip() # Clear whitespace
             if self.source[line] == "": # Ideally remove!
                 continue
-            elif self.source[line] == "}": # End of _VAR scope | Return index to skip lines
+            elif self.source[line] == "}": # End of _VAR scope | Returns index to skip lines
                 self.start_index = line+1
                 break
             #self.lastType = self.source[line].find([i for i in self.types]) != -1
@@ -92,7 +115,7 @@ class Program:
                     self.lastType = i
             f_index = self.source[line].find(":")
             if f_index == -1:
-                print("Invalid syntax, used datatype without variable specifier | on line: ", line) # Convert to format print
+                print(f"Invalid syntax, used datatype without variable specifier | on line: {line}") # Convert to format print
                 return False
             current_var_name : str = ""
             current_value : str = ""
@@ -109,18 +132,7 @@ class Program:
         return True
 
     def operator_calc(self, cur_line : str, index : int, op_code : int): 
-        #print(op_code)
         # Account for -> and <-
-        #if op_code > 3: # NOT: +, -, /, *
-            #var : str = ""
-            #for i in range(index, 0, -1): 
-                #if cur_line[i].isalnum():
-                    #var += cur_line[i]
-            #match op_code:
-                #case 4: self.variables[var[::-1]] += 1 # ++
-                #case 5: self.variables[var[::-1]] -= 1 # --
-            #return -1
-        # Standard operators (Two operands)
         # Change to allow variable + raw value!
         var_1 : str = ""
         var_2 : str = ""
@@ -128,12 +140,13 @@ class Program:
         found_s_var : bool = False
         f_type : int = 0 # Defines type | 0 = INT, 1 = FLOAT, ... 
         s_type : int = 0  
-        for var in self.variables: # Check for variables
-            position : int = cur_line.find(var)
-            if position != -1 and position < index: # Left side
+        for var in self.variables: 
+            l_position : int = cur_line.find(var) # Allows for same variable
+            r_position : int = cur_line.rfind(var)
+            if l_position != -1 and l_position < index: # Left side
                 var_1 = var
                 found_f_var = True
-            elif position != -1 and position > index: # Right side
+            if r_position != -1 and r_position > index: # Right side
                 var_2 = var
                 found_s_var = True
             if found_f_var and found_s_var: 
@@ -142,7 +155,7 @@ class Program:
             for i in range(index-1, -1, -1):
                 if cur_line[i].isdigit():
                     var_1 += cur_line[i]
-                elif cur_line[i] == '.': # Change!
+                elif cur_line[i] == '.': # Change! Float check
                     f_type = 1
             for i in range(index+1, len(cur_line)):
                 if cur_line[i].isdigit():
@@ -191,7 +204,6 @@ class Program:
                         case 1: var_2 = float(var_2)
                     return var_1 * var_2
             case 4: # ++
-                print("PLUS PLUS")
                 if found_f_var: 
                     self.variables[var_1] += 1
                     return self.variables[var_1]
@@ -216,21 +228,20 @@ class Program:
         lineCount : int = 0
         for line in range(self.start_index, len(self.source)):
             is_print_line : bool = False # Used for printing anything post "put"
+            line_output : str = ""
             f_index : int = -1
             self.source[line] = self.source[line].strip() # Clear starting and ending whitespace
             #self.source[lineCount] = self.source[line] # Rewrite to source
             if self.source[line].startswith('/'): # COMMENT CHECK
+                lineCount += 1
                 continue
             # ================== COMMANDS ==================
             index = self.source[line].find("put") # Reorder to allow line skip?
             if index != -1: 
                 is_print_line = True
-                output : str = ""
                 for i in range(index+3, len(self.source[line])):
-                    if self.source[line][i] == '"' and self.source[line][len(self.source[line])-2] == '"': #UPDATE?
-                        for j in range(i+1, len(self.source[line])-2):
-                            output += self.source[line][j]
-                #print(is_print_line)
+                    if self.source[line][i] != ' ':
+                        line_output += self.source[line][i]
             if self.source[line].find("wipe") != -1:
                 for i in self.variables.keys():
                     if self.source[line].find(i):
@@ -259,46 +270,62 @@ class Program:
                 ...
             # ================== OPERATORS ==================
             # Scan for operators: +, -, /, *, ++, --, etc.
-            for i in range(0, len(self.operators)):
+            for i in range(0, 4): # Only scan through identifiers
                 index : int = self.source[line].find(self.operators[i])
                 if index != -1: 
                     op_code : int = -1 # Pattern match
                     match self.source[line][index]:
                         case '+':
+                            op_code = OPERATOR_TOKENS.ADD
                             match self.source[line][index+1]:
-                                case '+': op_code = self.OP_TK.ADD_D;  break
-                                case '=': op_code = self.OP_TK.ADD_EQ; break
-                            op_code = self.OP_TK.ADD
+                                case '+': op_code = OPERATOR_TOKENS.ADD_D
+                                case '=': op_code = OPERATOR_TOKENS.ADD_EQ
                         case '-':
-                            match self.source[line][index+1]: # Account for other possible operators
-                                case '>': op_code = self.OP_TK.FN_RET; break
-                                case '=': op_code = self.OP_TK.SUB_EQ; break
+                            op_code = OPERATOR_TOKENS.SUB
+                            match self.source[line][index+1]: 
+                                case '>': op_code = OPERATOR_TOKENS.FN_RET
+                                case '=': op_code = OPERATOR_TOKENS.SUB_EQ
                             match self.source[line][index-1]:
-                                case '<': op_code = self.OP_TK.RET; break
-                            op_code = self.OP_TK.SUB
+                                case '<': op_code = OPERATOR_TOKENS.RET
                         case '/':
+                            op_code = OPERATOR_TOKENS.DIV
                             match self.source[line][index+1]:
-                                case '=': op_code = self.OP_TK.DIV_EQ; break
-                            op_code = self.OP_TK.DIV
+                                case '=': op_code = OPERATOR_TOKENS.DIV_EQ
                         case '*':
+                            op_code = OPERATOR_TOKENS.MUL
                             match self.source[line][index+1]:
-                                case '=': op_code = self.OP_TK.MUL_EQ; break
-                            op_code = self.OP_TK.MUL
+                                case '=': op_code = OPERATOR_TOKENS.MUL_EQ
                         case '=':
-                            if self.source[line][index+1] == '=': op_code = self.OP_TK.IS_EQ; break
-                            op_code = self.OP_TK.EQ
-                    result = self.operator_calc(self.source[line], index, op_code)
-                    if is_print_line:
-                        print(result)
+                            op_code = OPERATOR_TOKENS.EQ
+                            if self.source[line][index+1] == '=': op_code = OPERATOR_TOKENS.IS_EQ
+                    line_output = self.operator_calc(self.source[line], index, op_code)
             # ================== FUNCTIONS ==================
             if self.source[line].find("_FN") != -1:
                 fn_name : str = "" # Make global
+                has_args : bool = False
+                arguments = []
                 for i in range(3, len(self.source[line])):
-                    if self.source[line][i] == '(':
+                    if self.source[line][i] == '(': # Argument search
+                        curArgument : str = ""
+                        for j in range(i+1, len(self.source[line])):
+                            if self.source[line][j] == ',': # New var
+                                arguments.append(curArgument)
+                                curArgument = ""
+                            elif self.source[line][j] == ' ':
+                                continue
+                            elif self.source[line][j] == ')':
+                                arguments.append(curArgument)
+                                has_args = True
+                                break
+                            else:
+                                curArgument += self.source[line][j]
+                    if has_args:
                         break
-                    fn_name += self.source[line][i]
-                functions[fn_name] = lineCount # Change, get lines in a global manner
-            # ================== FN EXECUTE ==================
+                    if self.source[line][i] != ' ': # Name
+                        fn_name += self.source[line][i]
+                new_function : FUNCTION_STORAGE = FUNCTION_STORAGE(fn_name, arguments, line)
+                self.functions.append(new_function)
+            # ================== FN EXECUTE ================== | REMOVE!
             if self.source[line].find("(") != -1 and self.source[line].find(")") != -1:
                 if self.source[line].find("_FN") == -1:
                     # Get name, call appropiate function
@@ -309,14 +336,18 @@ class Program:
                         fn_name += self.source[line][i]
                     #self.function_call(functions[fn_name]) # Run function
             lineCount += 1
+            if is_print_line:
+                if line_output in self.variables:
+                    print(f"{self.variables[line_output]}")
+                else:
+                    print(f"{line_output}")
         return True
 
     def execute(self) -> bool:
         # Iterates over stored assets and computes
         #execute_queue : queue = queue.Queue(20) # Max func size of 20
-        for func in functions.values():
-            #execute_queue.append(func)
-            self.function_call(func)
+        for func in self.functions:
+            func.EXECUTE_FUNCTION()
         return False
 
 def main() -> None:
